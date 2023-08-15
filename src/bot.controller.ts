70495a7c74e0f3e5df08a533611e751387952a45
Marcos Volpi <marcos@marcosvolpi.com>
2023-08-15 15:39:11 -0300

    MP-70: Add the rest of the endpoint's responses

--- src/bot.controller.ts
+++ src/bot.controller.ts
@@ -27,16 +27,28 @@ export class BotController {
 		return this.botService.bot()
 	}
 
+	@ApiOkResponse({
+		description: 'The bot records',
+		type: Bot,
+	})
 	@Get('api/user/real-estate/bot/:id')
 	botById(@Param('id') id: string): Promise<SearchModel> {
 		return this.botService.botById({ id: Number(id) })
 	}
 
+	@ApiOkResponse({
+		description: 'The bot records',
+		type: Bot,
+	})
 	@Delete('api/user/real-estate/bot/:id')
 	delete(@Param('id') id: string): Promise<SearchModel> {
 		return this.botService.delete({ id: Number(id) })
 	}
 
+	@ApiOkResponse({
+		description: 'The bot records',
+		type: Bot,
+	})
 	@Post('api/user/real-estate/bot/')
 	create(
 		@Body()
