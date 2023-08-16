7566f2fd5bc8cca68669feb3153f6a4c996a7475
Marcos Volpi <marcos@marcosvolpi.com>
2023-08-16 10:24:03 -0300

    MP-71: Add city field

---
+++ prisma/migrations/20230816101833_add_city_field_to_result_table/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "Result" ADD COLUMN     "city" TEXT NULL;
---
+++ prisma/migrations/20230816132357_add_city_field_to_result_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `city` on the `Result` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Result" DROP COLUMN "city";
