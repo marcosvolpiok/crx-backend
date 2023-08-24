ecdddeac50e67dcdc7caec4108fe4637bf000cc5
Marcos Volpi <marcos@marcosvolpi.com>
2023-08-24 13:03:20 -0300

    Basic configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+// eslint-disable-next-line func-style
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true }),
+    config = new DocumentBuilder()
+      .setTitle('Legaltech')
+      .setDescription('Legaltech backend')
+      .setVersion('1.0')
+      .addTag('legaltech')
+      .build(),
+    document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = 5001;
+  await app.listen(port);
 }
 bootstrap();
